aed487475de59cdf4bc3607747dc7a7b9e5a99ae
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2021-08-08 22:33:48 +0500

    create models and run migrations

--- group/models.py
+++ group/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
+# pip install misaka
+import misaka
 
-# Create your models here.
+User = get_user_model()
+from django import template
+
+register = template.Library()
+
+class Group(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(allow_unicode=True, unique=True)
+    description = models.TextField(blank=True, default='')
+    desciption_html = models.TextField(editable=False, default='', blank=True)
+    members = models.ManyToManyField(User, through='GroupMember')
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        self.desciption_html = misaka.html(self.description)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('group:single', kwargs={'slug': self.slug})
+
+    class Meta:
+        ordering = ('name',)
+
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(Group, related_name='membership', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='user_group', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
